6bedf2b7ac9dde3c1d9a4b8266e355c8b5a5fba3
Carol Chung <cch5ng@gmail.com>
2021-06-28 13:26:05 -0700

    style the init game board

--- src/components/Board1.js
+++ src/components/Board1.js
@@ -1,3 +1,8 @@
+import iconPaper from '../images/icon-paper.svg';
+import iconRock from '../images/icon-rock.svg';
+import iconScissors from '../images/icon-scissors.svg';
+import './Board1.css';
+
 const Board1 = ({handleUserChoice}) => {
 
   const handleUserClick = (ev) => {
@@ -9,9 +14,19 @@ const Board1 = ({handleUserChoice}) => {
 
   return (
     <div className="game_board1">
-      <div id="paper" onClick={handleUserClick}>paper</div>
-      <div id="scissors" onClick={handleUserClick}>scissors</div>
-      <div id="rock" onClick={handleUserClick}>rock</div>
+      <div className="row_top">
+        <div id="paper" className="choice_icon paper_icon" onClick={handleUserClick}>
+          <img src={iconPaper} alttext="paper icon" />
+        </div>
+        <div id="scissors" className="choice_icon scissors_icon" onClick={handleUserClick}>
+          <img src={iconScissors} alttext="scissors icon" />
+        </div>
+      </div>
+      <div className="row_bottom">
+        <div id="rock" className="choice_icon rock_icon" onClick={handleUserClick}>
+          <img src={iconRock} alttext="rock icon" />
+        </div>
+      </div>
 
     </div>
   )
