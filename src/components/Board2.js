46fa97a4c7c9e2e12d78cc6bdd43aaa22eaab2d4
Carol Chung <cch5ng@gmail.com>
2021-06-29 09:38:34 -0700

    tried use react.useCallback but it broke everything; getWinner, getComputerChoice, and useEffect

--- src/components/Board2.js
+++ src/components/Board2.js
@@ -1,9 +1,14 @@
-import {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import iconPaper from '../images/icon-paper.svg';
 import iconRock from '../images/icon-rock.svg';
 import iconScissors from '../images/icon-scissors.svg';
 import './Board1.css';
+
+const choiceMap = new Map();
+choiceMap.set(0, 'rock');
+choiceMap.set(1, 'paper');
+choiceMap.set(2, 'scissors')
 
 const iconsKey = {
   paper: iconPaper,
@@ -11,12 +16,82 @@ const iconsKey = {
   scissors: iconScissors,
 }
 
-const Board2 = ({userChoice, resetGame, appendScore, 
-  computerChoice, winner, getWinner, getComputerChoice}) => {
+const Board2 = ({userChoice, resetGame, appendScore 
+  }) => { //computerChoice, winner, getWinner, getComputerChoice
+
+    const [computerChoice, setComputerChoice] = useState('');
+    const [winner, setWinner] = useState('');
+  
+
+      // /*
+// Paper beats Rock
+// - Rock beats Scissors
+// - Scissors beats Paper
+// */
+
+//faster to evaluated against sorted array if sorted array deep equals key, then get the winner
+const getWinner = React.useCallback(
+  (userChoice, computerChoice) => {
+    let curWinner;
+    if (userChoice === computerChoice) {
+      curWinner = 'none';
+    }
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        curWinner =  'computer'
+      } else if (computerChoice === 'scissors') {
+        curWinner =  'user'
+      }
+    }
+    if (userChoice === 'paper') {
+      if (computerChoice === 'rock') {
+        curWinner =  'user'
+      } else if (computerChoice === 'scissors') {
+        curWinner =  'computer'
+      }
+  
+    }
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        curWinner =  'computer'
+      } else if (computerChoice === 'paper') {
+        curWinner =  'user'
+      }
+    }
+  
+    setWinner(curWinner);
+    if (curWinner === 'user') {
+      appendScore();
+    }
+  }, [appendScore]
+)
+
+
+    const getComputerChoice = React.useCallback(
+      () => {
+        const numChoice = Math.floor(Math.random() * 3);
+        if (choiceMap.has(numChoice)) {
+          let compChoice = choiceMap.get(numChoice);
+          console.log('compChoice', compChoice);
+          setComputerChoice(compChoice);
+          getWinner(userChoice, compChoice);
+        }
+      }, [getWinner, userChoice])
+
+
+
+  const resetBoard = () => {
+    setWinner('');
+    setComputerChoice('');
+    resetGame();
+  }
 
   useEffect(() => {
+    console.log('gets to board2')
     getComputerChoice();
-  }, [getComputerChoice]);
+  }, [getComputerChoice]); //getComputerChoice
+
+  getComputerChoice();
 
   let userChoiceDivClass = `choice_icon icon_left ${userChoice}_icon`;
   let computerChoiceDivClass = `choice_icon icon_right ${computerChoice}_icon`;;
@@ -43,7 +118,7 @@ const Board2 = ({userChoice, resetGame, appendScore,
       {winner.length > 0 && (
         <div className="center">
           <div className="text_outcome">YOU {winner === 'user' ? 'WIN' : 'LOSE'}</div>
-          <button className="btn btn_secondary" onClick={resetGame}>Play Again</button>
+          <button className="btn btn_secondary" onClick={resetBoard}>Play Again</button>
         </div>
       )}
     </div>
