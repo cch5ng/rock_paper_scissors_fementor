9d697a52352f0c6050ef35bd7d727a74a5704dd3
Carol Chung <cch5ng@gmail.com>
2021-06-30 10:03:26 -0700

    15 readme (#21)
    * cleanup
    
    * update readme
    
    * update readme

--- src/App.js
+++ src/App.js
@@ -23,13 +23,6 @@ function App() {
           <GameBoard toggleModal={toggleModal} />
         )}      
       </main>
-      {/* <footer>
-        <div class="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-          Coded by <a href="#">Your Name Here</a>.
-        </div>
-
-      </footer> */}
       </div>
   );
 }
